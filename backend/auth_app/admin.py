a16aa1ad2a23a1d562114c1fe01aa7fc3ecea29d
Mikiejoe <omoshjoe02@gmail.com>
2025-02-12 01:45:46 +0300

    update site headers

--- backend/auth_app/admin.py
+++ backend/auth_app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from . import models
 
+
+admin.site.site_header = "Elimika Admin"
+admin.site.site_title = "Elimika Admin"
+admin.site.index_title = "Elimika Admin"
+
+
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'firstname', 'lastname', 'type', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email', 'firstname', 'lastname')
